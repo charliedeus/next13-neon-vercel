0322c3bafbcec8185d86dde30cd3a0a0ceece82d
Charles Loureiro de Deus <charliedeus@gmail.com>
2023-07-19 10:02:30 -0300

    users list and create

---
+++ drizzle.config.ts
@@ -0,0 +1,18 @@
+import { config } from 'dotenv'
+import type { Config } from 'drizzle-kit'
+import { z } from 'zod'
+
+config({
+  path: '.env.development.local',
+})
+
+const databaseUrl = z.string().url().parse(process.env.DATABASE_URL)
+
+export default {
+  schema: './src/db/schema/*',
+  out: './drizzle',
+  driver: 'pg',
+  dbCredentials: {
+    connectionString: databaseUrl,
+  },
+} satisfies Config
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,48 @@
-export default function Home() {
-  return <h1>Hello World</h1>
+import { db } from '@/db'
+import { users } from '@/db/schema'
+import { revalidatePath } from 'next/cache'
+
+export default async function Home() {
+  const allUsers = await db.select().from(users)
+
+  async function addUser(data: FormData) {
+    'use server'
+    console.log(data)
+
+    const fullName = data.get('fulll_name')?.toString()
+    const phone = data.get('phone')?.toString()
+
+    if (!fullName) {
+      return
+    }
+
+    await db.insert(users).values({
+      fullName,
+      phone,
+    })
+
+    revalidatePath('/')
+  }
+
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center gap-6 bg-zinc-900 text-zinc-50">
+      <p>{JSON.stringify(allUsers, null, 2)}</p>
+      <form action={addUser} className="flex flex-col gap-3">
+        <input
+          type="text"
+          name="fulll_name"
+          placeholder="Full name"
+          className="text-zinc-900"
+        />
+        <input
+          type="text"
+          name="phone"
+          placeholder="Phone"
+          className="text-zinc-900"
+        />
+
+        <button type="submit">Create</button>
+      </form>
+    </div>
+  )
 }
